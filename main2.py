0fd60952c4382bbf981c88a827976feeaa45c40e
Sikkandhar Batcha J <99872239+sssbatcha@users.noreply.github.com>
2026-08-09 11:05:57 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,9 @@
+def fib(n):
+    fib_series = [0,1]
+    while len(fib_series)< n:
+        fib_series.append(fib_series[-1]+fib_series[-2])
+    return fib_series[:n]
+n = int(input())
+result = fib(n)
+print(f"The first {n} numbers in the Fibonacci series are:{result}")
+
