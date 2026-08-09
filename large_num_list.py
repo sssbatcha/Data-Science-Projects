877c13d2d2ff768ada9aebbb986bc4f22e60c2fa
sssbatcha <99872239+sssbatcha@users.noreply.github.com>
2026-08-09 19:37:05 +0530

    new programs

--- large_num_list.py
+++ large_num_list.py
@@ -1,4 +1,8 @@
 numbers = list(map(int, input("Enter numbers with space:").split()))
 print(numbers)
-large=max(numbers)
+#large=max(numbers)
+large=numbers[0]
+for i in numbers:
+    if i>large:
+        large=i
 print(large)
