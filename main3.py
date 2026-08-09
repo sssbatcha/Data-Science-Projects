b2ba1f115deac7bdc141b1113745c301dc81c310
sssbatcha <99872239+sssbatcha@users.noreply.github.com>
2026-08-09 15:00:41 +0530

    modified

--- main3.py
+++ main3.py
@@ -1,4 +1,4 @@
-def find_max(num1,num2,num3):
+'''def find_max(num1,num2,num3):
     maximum = max(num1,num2,num3)
     return maximum
 
@@ -8,4 +8,22 @@ num1 = float(input("Enter the num1:"))
 num2 = float(input("Enter the num2:"))
 num3 = float(input("Enter the num3:"))
 maximum = find_max(num1,num2,num3)
+print(f"The Max of {num1},{num2},{num3} is :{maximum}")'''
+
+def find_max(num1,num2,num3):
+    if (num1 > num2 and  num1>num3):
+        maximum = num1
+    elif (num2 > num1 and  num2 > num3):
+        maximum = num2
+    else:
+        maximum = num3
+    return maximum
+
+
+
+num1 = float(input("Enter the num1:"))
+num2 = float(input("Enter the num2:"))
+num3 = float(input("Enter the num3:"))
+maximum = find_max(num1,num2,num3)
 print(f"The Max of {num1},{num2},{num3} is :{maximum}")
+
